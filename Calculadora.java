7a0eac9272fa551db6df4d4a2f338abd8f01c476
MiguelLorenzoLima <miguel.lorenzo.lb@gmail.com>
2026-08-03 21:30:20 -0300

    calculadora de operadores

--- Calculadora.java
+++ Calculadora.java
@@ -1,5 +1,23 @@
 package javaADS;
+
+import java.util.Scanner;
+
 public class Calculadora {
-    
+    public static void main(String[] args) {
+        Scanner numero1 = new Scanner (System.in);
+        System.out.println( "Digite o primeiro número: ");
+
+        Scanner numero2 = new Scanner (System.in);
+        System.out.println( "Digite o segundo número: ");
+
+        int a = numero1.nextInt();
+        int b = numero2.nextInt();
+
+        System.out.println("A soma é = " + (a + b));
+        System.out.println("A subtração é = " + (a - b));
+        System.out.println("A divisão é = " + (a / b));
+        System.out.println("A multiplicação é = " + (a * b));
+        System.out.println("O resto da divisão é = " + (a % b));
+
+        }
 }
-des 
