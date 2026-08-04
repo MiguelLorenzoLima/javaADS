2cda80832e9e4e69e9d942d82c2b51024b8bc22d
MiguelLorenzoLima <miguel.lorenzo.lb@gmail.com>
2026-08-03 21:57:29 -0300

    calculadoraDeOperadoresComErrosTratados

--- Calculadora.java
+++ Calculadora.java
@@ -1,17 +1,20 @@
 package javaADS;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculadora {
     public static void main(String[] args) {
-        Scanner numero1 = new Scanner (System.in);
+       
+       try{
+        Scanner numero = new Scanner (System.in);
+
+
         System.out.println( "Digite o primeiro número: ");
-
-        Scanner numero2 = new Scanner (System.in);
+        double a = numero.nextInt();
+        
         System.out.println( "Digite o segundo número: ");
-
-        int a = numero1.nextInt();
-        int b = numero2.nextInt();
+        double b = numero.nextInt();
 
         System.out.println("A soma é = " + (a + b));
         System.out.println("A subtração é = " + (a - b));
@@ -19,5 +22,11 @@ public class Calculadora {
         System.out.println("A multiplicação é = " + (a * b));
         System.out.println("O resto da divisão é = " + (a % b));
 
+        numero.close();
+       } catch (InputMismatchException  e){ // esse aqui é pra usuário burro que coloca valores inválidos
+        System.out.println("Digite um número válido");
+       } catch (ArithmeticException e) {
+            System.out.println("Erro: não é possível dividir por zero.");
         }
+     }
 }
