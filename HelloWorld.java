7a94d26f1ad11c31f17488e47f23c8d8079ce767
MiguelLorenzoLima <miguel.lorenzo.lb@gmail.com>
2026-08-03 21:18:17 -0300

    fisrt commit

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+package javaADS;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        System.out.println( "Digite a nota do aluno: ");
+
+        int a = entrada.nextInt();
+        if (a  >= 7 ){
+            System.out.println("aprovado");
+        } else {
+            System.out.println("reprovado");
+        }
+}
+}
